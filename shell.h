5835d3eab453277b448bb094922e5b2d9066120c
Muoti5 <mbathamuoti19@students.ku.ac.ke>
2023-10-05 01:48:36 -0700

    Shell

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H_
+#define SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+char *read_command(void);
+
+#endif
