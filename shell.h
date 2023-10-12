4ed6d97ff79ea6ba6fef451a56709b8f892fbd04
ritaOmondi <cherylritaomondi@gmail.com>
2023-10-12 01:52:58 -0700

    edited prototypes

--- shell.h
+++ shell.h
@@ -7,6 +7,8 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdbool.h>
+#include <stddef.h>
 
 extern char **environ;
 
@@ -16,6 +18,9 @@ void handle_error(void);
 void execute_and_wait(char *command, char *args[]);
 void execute_command(char *command);
 void free_args(char *args[]);
+char *my_getline();
+void exit_command(char *args[], int arg_count, char *command);
+void _strtok(char *input, const char *delimiters, char **tokens, int *num_tokens);
 void exit_shell(void);
 void print_environment(void);
 char *_getenv(const char *name);
