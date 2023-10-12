e2b64c9b64802d4e6e07c134a32edc1a83f0d578
ritaOmondi <cherylritaomondi@gmail.com>
2023-10-12 01:51:38 -0700

    Updated header file

--- shell.h
+++ shell.h
@@ -7,8 +7,8 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdbool.h>
-#include <stddef.h>
+
+extern char **environ;
 
 char *read_command(void);
 void execute_child(char *command, char *args[]);
@@ -16,7 +16,13 @@ void handle_error(void);
 void execute_and_wait(char *command, char *args[]);
 void execute_command(char *command);
 void free_args(char *args[]);
-char *my_getline();
-void exit_command(char *args[], int arg_count, char *command);
-void _strtok(char *input, const char *delimiters, char **tokens, int *num_tokens);
+void exit_shell(void);
+void print_environment(void);
+char *_getenv(const char *name);
+void handle_exec_error(char* command);
+char* construct_full_path(char* directory, char* command);
+void execute_with_path(char* command, char *args[]);
+char** create_exec_args(char* args[]);
+void free_exec_args(char** exec_args);
+
 #endif
